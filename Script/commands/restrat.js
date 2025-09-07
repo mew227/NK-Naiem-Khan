1f822072a2e9b6862d1c720da9e1830640cbee71
naiem42 <sadnaiemkhan@gmail.com>
2025-09-08 03:30:37 +0600

    Update restrat.js

--- Script/commands/restrat.js
+++ Script/commands/restrat.js
@@ -11,5 +11,13 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	const { threadID, messageID } = event;
-	return api.sendMessage(`${global.config.BOTNAME} Bot are now Restarting...`, threadID, () => process.exit(1));
+	return api.sendMessage(`${global.config.BOTNAME} 🤖 𝐁𝐨𝐭 𝐢𝐬 𝐍𝐨𝐰 𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐢𝐧𝐠 🔄⚡
+ ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+⚠️𝚂𝚢𝚜𝚝𝚎𝚖 𝚆𝚘𝚛𝚔𝚒𝚗𝚐 𝙽𝚘𝚠...
+𝚁𝚎𝚋𝚘𝚘𝚝𝚒𝚗𝚐 [■□□□□] 20%
+𝚁𝚎𝚋𝚘𝚘𝚝𝚒𝚗𝚐 [■■□□□] 40%
+𝚁𝚎𝚋𝚘𝚘𝚝𝚒𝚗𝚐 [■■■□□] 60%
+𝚁𝚎𝚋𝚘𝚘𝚝𝚒𝚗𝚐 [■■■■□] 80%
+𝚁𝚎𝚋𝚘𝚘𝚝𝚒𝚗𝚐[■■■■■] 100%
+𝙵𝚒𝚗𝚊𝚕𝚢 𝙱𝚘𝚝 𝚁𝚎𝚜𝚝𝚊𝚛𝚝𝚎𝚍 ✔︎`, threadID, () => process.exit(1));
 }
